e32f71361a2f45e7f87f77c0e2cb2fa5b2eb498f
David Masek <masd04@vse.cz>
2024-01-06 17:13:13 +0100

    back arrow and nav fix1

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { back  } from "../../public/img";
 
 const GoBack = () => {
@@ -9,7 +9,7 @@ const GoBack = () => {
         <div className="mb-6">
         <button className="scale-100 hover:scale-110" type="button" onClick={() => router.back()}>
           <Image
-          src={back}
+          src={back as StaticImageData}
           alt='Go back button'
           width={50}
           height={40}          
