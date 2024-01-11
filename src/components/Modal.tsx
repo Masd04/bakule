3c756d19ba9883e566b15d71be63cde98ab93a08
David Masek <masd04@vse.cz>
2024-01-11 22:28:12 +0100

    responsivity modal, h elemnt, tailwindconfig

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,35 +15,38 @@ const Modal: React.FC<ModalProps> = ({ isVisible, onClose }) => {
     e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
   };
 
+
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
       <div className="w-[80%] flex flex-col bg-white px-5 pb-5 pt-2 rounded-md shadow-lg">
-        <button className="place-self-end mb-4 hover:scale-105" onClick={onClose}>
-        <Image
+        
+      <div className="flex items-center justify-between">
+        <h2 className="se:text-md xr:text-lg promax:text-xl sm:text-2xl md:text-3xl ml-10 font-bold text-center flex-grow">Rate your community!</h2>
+        <button className="ml-auto hover:scale-105" onClick={() => onClose() } aria-label="Close">
+        <Image 
           src={closeButton as StaticImageData}
           alt='Go back button'
-          width={50}
-          height={40}          
+          width={40}
+          height={40}
           />
         </button>
+      </div>
 
-        <div className="flex justify-center">
-        <h2 className="font-bold text-3xl mb-2">Rate your community!</h2>
-        </div>
 
         <form className="flex flex-col space-y-4">
           <div>
-            <label htmlFor="value1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Rating (1-10)</label>
-            <input type="number" id="value1" min="1" max="10" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+            <label htmlFor="value1" className="block mb-2 text-sm md:text-lg font-medium text-gray-900 dark:text-gray-300">Rating (1-10)</label>
+            <input type="number" id="value1" min="1" max="10" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm md:text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
           </div>
 
           <div>
-            <label htmlFor="value2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Review</label>
+            <label htmlFor="value2" className="block mb-2 text-sm md:text-lg font-medium text-gray-900 dark:text-gray-300">Review</label>
             <textarea 
               id="value2" 
               maxLength={200} 
               rows={1}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm md:text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               style={{ overflowY: 'hidden', resize: 'none' }}
               onInput={handleInput}
             />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        se: "374px",
+        xr: "413px",
+        promax: "429px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
