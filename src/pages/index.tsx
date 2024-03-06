c54d87da7f1319c8c9ccebb001a5d8cd0d77a963
David Masek <masd04@vse.cz>
2024-03-06 22:50:46 +0100

    add community button + select-none gone form code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,17 +21,18 @@ const Communities: NextPage = () => {
   return (
   <>
   <div className="bg-gray-100 pt-3">
+      <div className="flex items-center justify-between px-4 md:px-6 lg:px-8">
+        <header className="flex-grow ml-52">
+          <h1 className="text-4xl font-bold text-black text-center bg-gray-100 px-4 pt-2 pb-3">Communities</h1>
+          <div className="flex justify-center py-2 bg-gray-100" id="srch">
+            <input type="text" placeholder="search" className="w-[14rem] bg-gray-300 border-2 border-gray-400 text-center rounded-full"/>
+          </div>
+        </header>
 
-    <header className="sticky top-[4.78rem] z-10 pt-1 mt-0">
-      
-      <h1 className="px-4 pt-2 pb-3 text-black bg-gray-100 text-4xl font-bold text-center">Communities</h1>
-
-      <div className="flex justify-center py-2 select-none bg-gray-100">
-        <input type="text" placeholder="search" className="bg-gray-300 rounded-full w-[14rem] text-center border-2 border-gray-400"/>
+        <button className="w-52 h-12 px-5 ml-4 bg-[#270858] rounded-md text-white font-bold hover:scale-105">
+          Add new community
+        </button>
       </div>
-
-    </header>
-
    
     {communities.map((community: Community) => (
                 <CommunityCard key={community.id} community={community} />
