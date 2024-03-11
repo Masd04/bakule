ec227da6884b487582c84ede3a403be02a911a29
David Masek <masd04@vse.cz>
2024-03-11 23:33:30 +0100

    modal main page

---
+++ src/utils/logger.ts
@@ -0,0 +1,20 @@
+// utils/logger.ts
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+interface LogDetails {
+  [key: string]: any;
+}
+
+export async function logDbCall(description: string, details: LogDetails) {
+  // Insert a log entry into your database
+  await prisma.log.create({
+    data: {
+      description,
+      details: details,
+      environment: process.env.NODE_ENV, // This will log 'development' or 'production'
+    },
+  });
+}
