f8ce152b10845221913ec17d5b19e81af578bf32
David Masek <masd04@vse.cz>
2024-04-18 20:40:51 +0200

    some styling

--- src/components/ModalAdd.tsx
+++ src/components/ModalAdd.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
 import { closeButton  } from "../../public/img";
+import { useSession } from "next-auth/react";
+import emailjs from "@emailjs/browser"
+
+emailjs.init({publicKey: 'hPcJNDRpyvCulCBHi',});
 
 interface ModalProps {
   isVisible: boolean;
   onClose: () => void;
 }
 
+interface ExtendedSession {
+  user: {
+    id: string;
+    name: string;
+  }
+}
+
 const ModalAdd: React.FC<ModalProps> = ({ isVisible, onClose }) => {
+  const [emailSent, setEmailSent] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  
+  const { data: session } = useSession() as { data: ExtendedSession };
+  const user = session?.user;
+
   if (!isVisible) return null;
 
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -15,6 +32,36 @@ const ModalAdd: React.FC<ModalProps> = ({ isVisible, onClose }) => {
     e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
   };
 
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+
+    const form = e.currentTarget;
+    const serverNameInput = form.elements.namedItem('value2') as HTMLTextAreaElement;
+  
+  
+    const emailParams = {
+      user_id: user.id,
+      user_name: user.name,
+      server_name: serverNameInput.value,
+    };
+  
+    emailjs.send('service_nm47c3f', 'template_9p79oos', emailParams)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setEmailSent(true);
+        setIsSubmitting(false);
+        setTimeout(() => {
+          setEmailSent(false);
+          onClose(); // Close the modal after showing the success message
+        }, 3000); // Show the success message for
+        
+      }, (err) => {
+        console.error('FAILED...', err);
+        setIsSubmitting(false);
+      });
+  };
 
 
   return (
@@ -33,7 +80,14 @@ const ModalAdd: React.FC<ModalProps> = ({ isVisible, onClose }) => {
         </button>
       </div>
 
-        <form className="flex flex-col space-y-4">
+      {emailSent ? (
+        <div className="flex justify-center" id="sentMessage">
+          <div className="w-[70%] text-center font-bold p-4 mt-4 mb-4 text-xl text-green-700 bg-green-100 rounded-md" role="alert">
+            Your request has been sent successfully!
+          </div>
+        </div>
+        ) : (
+        <form className="flex flex-col space-y-4" onSubmit={sendEmail}>
           <div>
             <label htmlFor="value2" className="block mb-2 text-sm md:text-lg font-medium text-gray-900 dark:text-gray-300">Server name:</label>
             <textarea 
@@ -46,10 +100,14 @@ const ModalAdd: React.FC<ModalProps> = ({ isVisible, onClose }) => {
             />
           </div>
 
-          <button className="rounded bg-[#270858] md:w-[25%] md:mx-auto pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-opacity-90 focus:outline-none focus:ring-0 active:bg-opacity-80">
-            Request
+          <button type="submit"
+                  className="rounded bg-[#270858] md:w-[25%] md:mx-auto pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-opacity-90 focus:outline-none focus:ring-0 active:bg-opacity-80"
+          >
+          {isSubmitting ? 'Sending...' : 'Request'}
           </button>
         </form>
+        )}
+        
       </div>
 
     </div>
