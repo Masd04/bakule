85284c12b9c00b5af67f6731fedcd4fe7bf6fbb5
David Masek <masd04@vse.cz>
2024-04-20 22:22:32 +0200

    profile loading page

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -4,26 +4,31 @@ import Head from "next/head";
 import styles from '../../styles/style.js';
 import { ssgHelper } from "~/server/api/ssgHelper";
 import { api } from "~/utils/api";
-import ErrorPage from "next/error";
 import Image from 'next/image';
 import  GoBack  from "~/components/GoBack";
 import AvgRate from "~/components/AvgRate";
+import Alert from "~/components/Alert";
 
 
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
      id, 
     }) => {
-     const { data: profile } = api.profile.getById.useQuery({ id })
-     const { data: userRatingsAndReviews } = api.profile.getUserRatingsAndReviews.useQuery({ userId: id });
+     const { data: profile, isLoading: isLoadingProfile, isError: isErrorProfile } = api.profile.getById.useQuery({ id })
+     const { data: userRatingsAndReviews, isLoading: isLoadingRatingsReviews, isError: isErrorRatingsReviews } = api.profile.getUserRatingsAndReviews.useQuery({ userId: id });
 
-     if (!profile || !userRatingsAndReviews) {
-       return <ErrorPage statusCode={404} />;
-     }
+     if (isLoadingProfile || isLoadingRatingsReviews) {
+        return <Alert message="Loading..." textColor="text-cpblue" />;
+      }
+    
+      // Combined error state
+      if (isErrorProfile || isErrorRatingsReviews) {
+        return <Alert message="Failed to load profile." textColor="text-cpred" />;
+      }
 
     return <>
     <Head>
-        <title>{`Profile ${profile.name}`}</title>
+        <title>{`Profile ${profile?.name}`}</title>
     </Head>
 
     <div className={`container mx-auto px-6 py-2`}>
@@ -33,18 +38,18 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
     <div className="bg-white rounded-lg shadow-lg border-2 border-gray-200">
     <div className={`${styles.profileTxt}`}>
     <div className={`${styles.flexCenter} w-1/3 sm:w-full mx-auto`}>
-    <Image src={profile.image ? profile.image : 'https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg'}
+    <Image src={profile?.image ? profile.image : 'https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg'}
            alt="Profile image" 
            width={200} 
            height={200} />
     </div>
-    <h1 className={`${styles.flexCenter}`}>{profile.name}</h1>
-    <h1 className={`${styles.flexCenter} font-light`}>{profile.email}</h1>
+    <h1 className={`${styles.flexCenter}`}>{profile?.name}</h1>
+    <h1 className={`${styles.flexCenter} font-light`}>{profile?.email}</h1>
     </div>
     </div>
 
     <div className="mt-5 mb-3 text-lg font-bold">
-      <h3>Your ratings:</h3>
+      <h3>Your ratings</h3>
     </div>
 
 
