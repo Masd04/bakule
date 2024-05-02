c5068a808a40958341b04612db425952db5abee8
David Masek <masd04@vse.cz>
2024-05-02 10:59:35 +0200

    sitemap3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,13 @@ const fetch = require('node-fetch');
 async function fetchPaths(url) {
     const res = await fetch(url);
     const data = await res.json();
-    return data.map(path => ({ loc: path }));
+    if (Array.isArray(data)) {
+        return data.map(path => ({ loc: path }));
+    } else if (data.paths && Array.isArray(data.paths)) {
+        return data.paths.map(path => ({ loc: path }));
+    } else {
+        throw new Error('Unexpected response format');
+    }
 }
 
 module.exports = {
