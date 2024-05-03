94f4eede4bc4c45662e58c0b50ca1ec4e3b38876
David Masek <masd04@vse.cz>
2024-05-03 02:06:48 +0200

    Lil styling ComCard

--- src/components/CommunityCard.tsx
+++ src/components/CommunityCard.tsx
@@ -16,7 +16,7 @@ export function CommunityCard({ community }: CommunityProps) {
     <Link href={`/communities/${community.id}`}>
     <div id="card" className="py-4 mx-6">
     <div
-    className="block bg-sky-50 p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    className="block bg-sky-50 p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
     
 
     <div className={`${styles.flexBtw} items-start`}>
